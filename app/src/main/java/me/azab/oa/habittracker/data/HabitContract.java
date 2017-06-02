7651c5545e659bdfbcd2ad3118858e71b31b8e39
Omar <oa.azab@gmail.com>
2017-06-02 14:29:39 +0200

    Define contarct class

---
+++ app/src/main/java/me/azab/oa/habittracker/data/HabitContract.java
@@ -0,0 +1,30 @@
+package me.azab.oa.habittracker.data;
+
+/**
+ * Created by omar on 6/2/2017.
+ */
+
+import android.provider.BaseColumns;
+
+/**
+ * Contract class defines the Habit tracker database
+ */
+public final class HabitContract {
+
+    // Private constructor
+    private HabitContract() {
+    }
+
+    /**
+     * Inner Class represent the Habit table in the database
+     */
+    public final static class HabitEntry implements BaseColumns {
+
+        // Table name
+        public static final String TABLE_NAME = "habits";
+
+        // Column names
+        public static final String _ID = BaseColumns._ID;
+        public final static String HABIT_COLUMN_NAME = "name";
+    }
+}
